4481a7578775c54b031f6de04fa154e8112d27d9
te230150-cyber <te230150@gmail.com>
2026-06-04 11:24:36 +0900

    例外実装

--- src/test/java/com/example/CalcTest.java
+++ src/test/java/com/example/CalcTest.java
@@ -1,40 +1,68 @@
 package com.example;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.assertj.core.api.Assertions.assertThatThrownBy;
+
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 
 public class CalcTest {
-    static Calc calc=null;
+    static Calc calc = null;
+
     @BeforeAll
-    static void テスト前処理(){
-        calc=new Calc();
-    }
-    @Test 
-    void addテスト正常(){
-        assertEquals(calc.add(1,3),4);
+    static void テスト前処理() {
+        calc = new Calc();
     }
 
     @Test
-    void divテスト正常(){
-        assertEquals(calc.div(5,4), 1);
+    void addテスト正常() {
+        assertEquals(calc.add(1, 3), 4);
     }
-
 
     @Test
-    void mulテスト正常(){
-        assertEquals(calc.mul(5,5),25);
+    void divテスト正常() {
+        assertEquals(calc.div(5, 4), 1);
     }
-    
+
+    @Test
+    void mulテスト正常() {
+        assertEquals(calc.mul(5, 5), 25);
+    }
+
+    @Test
+    void divテスト異常() {
+        assertThatThrownBy(() -> {
+            calc.div(4, 0);
+        })
+                .isInstanceOf(ArithmeticException.class)
+                .hasMessageContaining("/ by zero");
+    }
+
     @Disabled
-    void addテスト異常(){
-        assertEquals(calc.add(3,3),33);
+    void addテスト異常() {
+        assertEquals(calc.add(3, 3), 33);
     }
 
-    
     @AfterAll
-    static void テスト後処理(){
+    static void テスト後処理() {
         calc = null;
     }
+
+
+
+
+    @Test
+    void 例外() {
+
+         assertThatThrownBy(() -> {
+        calc.div(4, 0);
+    })
+    //発生したexceptionのクラス
+    .isExactlyInstanceOf(ArithmeticException.class)
+    //エラーメッセージに特定の文字列が含まれるか
+    .hasMessageContaining("/by zero");
+    }
+
 }
